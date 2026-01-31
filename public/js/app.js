c7d76219b28554406df0669f598a34370204346f
Tiago <tiagogn2004@gmail.com>
2026-01-30 21:39:37 -0500

    Added images, loading each city card, and other fixes

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,36 @@
-console.log("Script loaded");
 
+// When page is loaded we load the content 
 document.addEventListener('DOMContentLoaded', () =>{
     loadContent();
 });
 
+
+// Calls API for all the cities
 async function loadContent(){
     try{
         const response = await fetch('/api/cities');
         const cities = await response.json();
-        
-        
+        display(cities);
     }catch(error){
         console.error("Cities couldnt load: ", error);
     }
+}
+
+
+// Goes through the list of cities and uses that information to build them a card
+function display(cities){
+    const container = document.querySelector('.card-container');
+    cities.forEach(city => {
+        const elem = document.createElement('div');
+        elem.classList.add('card');
+        console.log(city.title)
+        elem.innerHTML = `
+            <img src="/images/${city.img}" alt="${city.title}">
+            <h3> ${city.title} </h3>
+            <p>${city.description}</p>
+        `;
+
+        container.appendChild(elem);
+    });
+
 }
--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const path = require('path');
 const PORT = 8080;
 
 let weather_library =[
-    {city: 'Toronto', weather: 'snowy', population:99999999999, gdp: 100000, description: "Its a city" }
+    {title: 'Toronto', weather: 'snowy', population:99999999999, gdp: 100000, img:'toronto.jpg', description: "Its a city" },
+    {title: 'Toronto', weather: 'snowy', population:99999999999, gdp: 100000, img:'toronto.jpg', description: "Its a city" },
+    {title: 'Toronto', weather: 'snowy', population:99999999999, gdp: 100000, img:'toronto.jpg', description: "Its a city" },
+    {title: 'Toronto', weather: 'snowy', population:99999999999, gdp: 100000, img:'toronto.jpg', description: "Its a city" }
+    
 ];
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 
+// These will be the links to our html pages
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
@@ -22,8 +27,12 @@ app.get('/charts', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/charts.html'));
 });
 
+
+// API 
 app.get('/api/cities', (req, res) => {
     res.status(200).json(weather_library);
     console.log(weather_library);
 });
+
+// Starts the server, listens on port 8080
 app.listen(PORT, () => {console.log("listening")});
