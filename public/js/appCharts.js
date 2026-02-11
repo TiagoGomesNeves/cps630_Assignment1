af870c386cba28a18c25966509ee6ab8193129c6
Tiago <tiagogn2004@gmail.com>
2026-02-11 14:23:41 -0500

    Added selectors for charts

--- public/js/appCharts.js
+++ public/js/appCharts.js
@@ -1,1 +1,25 @@
-console.log();
+document.addEventListener('DOMContentLoaded', () =>{
+    loadContent();
+});
+
+async function loadContent(){
+    try{
+        const response = await fetch('/api/cities/title');
+        const cities = await response.json();
+
+        const container = document.getElementById('selector');
+        const container2 = document.getElementById('selector2');
+        cities.forEach(element => {
+            let option = document.createElement('option');
+            option.value = element;
+            option.textContent = element; 
+            let option2 = document.createElement('option');
+            option2.value = element;
+            option2.textContent = element; 
+            container.append(option);
+            container2.append(option2);
+        });
+    }catch(error){
+        console.error("Error: ", error);
+    }
+}
